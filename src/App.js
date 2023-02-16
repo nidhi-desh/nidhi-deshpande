e514df8dd8a055673453684809061d2ad0c4195b
Nidhi <nidhideshpande08@gmail.com>
2023-02-15 17:08:35 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import './App.scss'
-import './Components/Layout'
-import { Route, Routes } from 'react-router-dom'
-import Layout from './Components/Layout'
-import Home from './Components/Home'
-import About from './Components/About'
-import Resume from './Components/Resume'
-import Contact from './Components/Contact'
+import "./App.scss";
+import "./Components/Layout";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Components/Layout";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Resume from "./Components/Resume";
+import Contact from "./Components/Contact";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/about" about element={<About />} />
-          <Route path="/resume" about element={<Resume />} />
+        <Route basename={process.env.PUBLIC_URL} path="/" element={<Layout />}>
+          <Route basename={process.env.PUBLIC_URL} index element={<Home />} />
+          <Route
+            basename={process.env.PUBLIC_URL}
+            path="/about"
+            about
+            element={<About />}
+          />
+          <Route
+            basename={process.env.PUBLIC_URL}
+            path="/resume"
+            about
+            element={<Resume />}
+          />
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
